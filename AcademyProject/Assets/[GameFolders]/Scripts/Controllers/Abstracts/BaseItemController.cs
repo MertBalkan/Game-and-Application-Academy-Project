86d53c0f1382a6d29673dd27f0c406b735f2a0e6
Mert Balkan <mert__balkan@hotmail.com>
2022-05-06 23:43:28 +0300

    Base item controller class added

---
+++ AcademyProject/Assets/[GameFolders]/Scripts/Controllers/Abstracts/BaseItemController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using AcademyProject.ScriptableObjects;
+using UnityEngine;
+
+namespace AcademyProject.Controllers
+{
+    public abstract class BaseItemController : MonoBehaviour
+    {
+        [SerializeField] private ItemDataSO itemDataSo;
+    }
+}
