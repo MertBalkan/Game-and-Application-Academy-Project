cd471406a9e71d147ed8604ec976eb02fbba1f09
Emre Yoldaş <53980583+emreyoldass@users.noreply.github.com>
2022-05-23 19:43:10 +0300

    lego&slime&deterjan

---
+++ AcademyProject/Assets/[GameFolders]/Scripts/Combats/Lego & slime/lego2.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lego2 : MonoBehaviour
+{
+    public float zamanYavaslatKatSayisi;
+    public float zamanYavaslatmaSuresi;
+    public bool girdimi;
+    public LayerMask player;
+    public float sightrange;
+    public bool playerInsightRange;
+    private void Update()
+    {
+        Time.timeScale += (1f / zamanYavaslatmaSuresi) * Time.unscaledDeltaTime;
+        Time.timeScale = Mathf.Clamp(Time.timeScale, 0f, 1f);
+       
+        playerInsightRange = Physics.CheckSphere(transform.position, sightrange, player);
+        if (playerInsightRange)
+        {
+            durdur();
+        }
+    }
+    void durdur()
+    {
+        if (!girdimi)
+        {
+
+            Time.timeScale = zamanYavaslatKatSayisi;
+            Time.fixedDeltaTime = Time.timeScale * 0.2f;
+            
+        }
+        resetleme();
+        girdimi = true;//kaldirirsak her girdiginde bu olay olacak true dersek tek seferlik calisir arkaplanda nolur bilemem
+
+    }
+    private void resetleme()
+    {
+        girdimi = false;
+    }
+}
