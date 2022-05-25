45ac262746ef003ae5b19e97ce74b3019423a007
Mert Balkan <mert__balkan@hotmail.com>
2022-05-25 23:30:18 +0300

    Namespace for time

--- AcademyProject/Assets/[GameFolders]/Scripts/UIs/Concretes/CountDown.cs
+++ AcademyProject/Assets/[GameFolders]/Scripts/UIs/Concretes/CountDown.cs
@@ -1,36 +1,38 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CountDown : MonoBehaviour
+namespace AcademyProject.UIs
 {
-    [SerializeField] private Image _time;
-    [SerializeField] private Text _timeText;
-    [SerializeField]private float _currentTime;
-    [SerializeField] private float _duration;
-    void Start()
+    public class CountDown : MonoBehaviour
     {
-        _currentTime = _duration;
-        float minute = Mathf.FloorToInt(_currentTime / 60);
-        float seconds = Mathf.FloorToInt(_currentTime % 60);
-        _timeText.text = string.Format("{0:00}:{1:00}", minute, seconds);
-        StartCoroutine(CountdownTime());
-        
-    }
-
-    private IEnumerator CountdownTime()
-    {
-        while (_currentTime >= 0)
+        [SerializeField] private Image _time;
+        [SerializeField] private Text _timeText;
+        [SerializeField]private float _currentTime;
+        [SerializeField] private float _duration;
+        void Start()
         {
-            _time.fillAmount = Mathf.InverseLerp(0, _duration, _currentTime);
+            _currentTime = _duration;
             float minute = Mathf.FloorToInt(_currentTime / 60);
             float seconds = Mathf.FloorToInt(_currentTime % 60);
             _timeText.text = string.Format("{0:00}:{1:00}", minute, seconds);
-            yield return new WaitForSeconds(1f);
-            _currentTime--;
-            _time.fillAmount = _currentTime;
+            StartCoroutine(CountdownTime());
+        
         }
-        yield return null;
+
+        private IEnumerator CountdownTime()
+        {
+            while (_currentTime >= 0)
+            {
+                _time.fillAmount = Mathf.InverseLerp(0, _duration, _currentTime);
+                float minute = Mathf.FloorToInt(_currentTime / 60);
+                float seconds = Mathf.FloorToInt(_currentTime % 60);
+                _timeText.text = string.Format("{0:00}:{1:00}", minute, seconds);
+                yield return new WaitForSeconds(1f);
+                _currentTime--;
+                _time.fillAmount = _currentTime;
+            }
+            yield return null;
+        }
     }
 }
