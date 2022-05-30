f02273db38d1da1bd1017d26aecc52492c0d977f
Mert Balkan <mert__balkan@hotmail.com>
2022-05-30 23:58:44 +0300

    Update WaveManager.cs

--- AcademyProject/Assets/[GameFolders]/Scripts/Managers/Concretes/WaveManager.cs
+++ AcademyProject/Assets/[GameFolders]/Scripts/Managers/Concretes/WaveManager.cs
@@ -1,8 +1,5 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using AcademyProject.Controllers;
-using AcademyProject.UIs;
 using UnityEngine;
 
 namespace AcademyProject.Managers
