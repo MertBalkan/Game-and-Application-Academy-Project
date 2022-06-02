54dc69f01d6e4f4f56c8a16dbedcbaf8aaa4511d
Mert Balkan <mert__balkan@hotmail.com>
2022-06-03 00:03:09 +0300

    Camera code cleanup

--- AcademyProject/Assets/[GameFolders]/Scripts/Controllers/Concretes/CharacterCameraController.cs
+++ AcademyProject/Assets/[GameFolders]/Scripts/Controllers/Concretes/CharacterCameraController.cs
@@ -12,6 +12,7 @@ namespace AcademyProject.Controllers
         [SerializeField] private float relativePositionToPlayer = 30.0f;
         [SerializeField] private float cameraSpeed;
         [SerializeField] private float  cameraScrollSpeed = 250.0f;
+        [SerializeField] private float cameraRotateSpeed = 20.0f;
         
         private CinemachineVirtualCamera _cinemachine;
         private PlayerCharacterController _player;
@@ -32,16 +33,7 @@ namespace AcademyProject.Controllers
             CamMovementDir(Vector3.back, _player.Input.VerticalMovement, -1);
 
             MouseScrollControl();
-
-            if (_player.Input.CamLeftMov)
-            {
-                transform.RotateAround(_player.transform.position, Vector3.up, 20 * Time.deltaTime);
-            }
-
-            if (_player.Input.CamRightMov)
-            {
-                transform.RotateAround(_player.transform.position, Vector3.up, 20 * Time.deltaTime);
-            }
+            CameraRotate();
         }
 
         private void CamMovementDir(Vector3 directionVector, float direction, float compareValue)
@@ -56,6 +48,15 @@ namespace AcademyProject.Controllers
             var camScrollVal = _player.Input.CameraScroll * cameraScrollSpeed * Time.deltaTime;
             var clampedVal = Mathf.Clamp(camScrollVal, 15, 60);
             _cinemachine.m_Lens.FieldOfView -= camScrollVal;
+        }
+
+        private void CameraRotate()
+        {
+            if (_player.Input.CamLeftMov)
+                transform.RotateAround(_player.transform.position, Vector3.up, cameraRotateSpeed * Time.deltaTime);
+
+            if (_player.Input.CamRightMov)
+                transform.RotateAround(_player.transform.position, Vector3.up, -cameraRotateSpeed * Time.deltaTime);
         }
     }
 }
