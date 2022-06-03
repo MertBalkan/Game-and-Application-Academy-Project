fe197b40151289304d399d690572309c68407abc
Emulz <setenayemulz@gmail.com>
2022-06-03 04:23:27 +0300

    Merged: MertBranch

---
+++ AcademyProject/Assets/[GameFolders]/Scripts/Observers/KeyObserver.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using AcademyProject.Controllers;
+using AcademyProject.Managers;
+using UnityEngine;
+
+namespace AcademyProject.Observers
+{
+    public class KeyObserver : MonoBehaviour
+    {
+        [SerializeField] private List<KeyController> allKeys;
+
+        private void Start()
+        {
+            WaveManager.Instance.OnWaveFinished += HandleOnWaveFinished;
+        }
+
+        private void OnDisable()
+        {
+            WaveManager.Instance.OnWaveFinished -= HandleOnWaveFinished;
+        }
+
+        private void HandleOnWaveFinished()
+        {
+            if(allKeys == null) return;
+            
+            for (int i = 0; i < 1 && allKeys.Capacity != 0; i++)
+            {
+                allKeys[i].gameObject.SetActive(true);
+                allKeys.Remove(allKeys[i]);
+            }
+        }
+    }
+}
