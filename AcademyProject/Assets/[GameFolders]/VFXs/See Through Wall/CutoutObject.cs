322d6b75086144ccb74137d2ee362fc0735d23b2
furkankaya0 <97740231+furkankaya0@users.noreply.github.com>
2022-06-04 19:01:10 +0300

    CutoutObjects

---
+++ AcademyProject/Assets/[GameFolders]/VFXs/See Through Wall/CutoutObject.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutoutObject : MonoBehaviour
+{
+    [SerializeField]
+    private Transform targetObject;
+
+    [SerializeField]
+    private LayerMask wallMask;
+
+    private Camera mainCamera;
+
+    private void Awake()
+    {
+        mainCamera = GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        Vector2 cutoutPos = mainCamera.WorldToViewportPoint(targetObject.position);
+        cutoutPos.y /= (Screen.width / Screen.height);
+
+        Vector3 offset = targetObject.position - transform.position;
+        RaycastHit[] hitObjects = Physics.RaycastAll(transform.position, offset, offset.magnitude, wallMask);
+
+        for (int i = 0; i < hitObjects.Length; ++i)
+        {
+            Material[] materials = hitObjects[i].transform.GetComponent<Renderer>().materials;
+
+            for (int m = 0; m < materials.Length; ++m)
+            {
+                materials[m].SetVector("_CutoutPos", cutoutPos);
+                materials[m].SetFloat("_CutoutSize", 0.1f);
+                materials[m].SetFloat("_FalloffSize", 0.05f);
+            }
+        }
+    }
+}
